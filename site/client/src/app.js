5ef18b24aba53c253d29b85b84b9647a2e9bb4fc
jamesnr56 <james.nevillerolfe@gmail.com>
2021-07-04 11:29:44 +0100

    setting up react-app [client]

---
+++ site/client/src/app.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+	// default/base function component
+	return (
+		<>
+			<div className="placeholder">App component</div>
+		</>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
