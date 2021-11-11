cc2553fd865637d8bc2c5b2101e810eca673937f
James Neville-Rolfe <71930043+jamesnrolfe@users.noreply.github.com>
2021-11-11 12:45:06 +0000

    Update get_users.js

--- site/server/routes/users/get_users.js
+++ site/server/routes/users/get_users.js
@@ -2,3 +2,22 @@ const express = require("express");
 const router = express.Router();
 const con = require("../../connection");
 
+router.post("/", (req, res) => {
+    // read data from the POST request
+    const id = req.body.id; // getting the user based on their id
+    // if no id is given, then get all users
+    
+    const sql = "";
+    if (id == null) {
+        // if we are not given an id
+        sql = "SELECT * FROM user_details"; // just get all users
+    } else {
+        // otherwise get the specific user
+        sql = "SELECT * FROM user_details WHERE user_id = ?";
+    }
+    
+    con.query(sql, id, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    }    
+}    
