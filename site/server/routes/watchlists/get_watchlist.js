3675086ee1ff2e4d3ce9a1f6a13410f3e04a683e
jamesnr56 <james.nevillerolfe@gmail.com>
2021-11-16 17:10:03 +0000

    Update get_watchlist.js

--- site/server/routes/watchlists/get_watchlist.js
+++ site/server/routes/watchlists/get_watchlist.js
@@ -5,11 +5,11 @@ const con = require("../../connection");
 router.post("/", (req, res) => {
 	const id = req.body.id; // will be used to fetch the user
 
-	const sql = "SELECT movie_id FROM user_watchlists WHERE user_id = ?";
+	const sql = "SELECT movie_id FROM user_watchlists WHERE user_id = ?"; // simple select statement
 
 	con.query(sql, id, (err, result) => {
 		if (err) throw err;
-		res.send(result);
+		res.send(result); // if no error, this should be an array of watchlist items
 	});
 });
 
