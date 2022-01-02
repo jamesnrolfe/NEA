450f46eafc5ef92d089b8663e02ae8d91ee29af4
jamesnr56 <james.nevillerolfe@gmail.com>
2022-01-02 17:25:30 +0000

    adding navbar and sidebar + button

---
+++ site/client/src/components/large/sidebar/index.js
@@ -0,0 +1,13 @@
+import Sidebar from "./sidebar"; // import the visual element
+
+const SidebarContainer = (props) => {
+	return (
+		<Sidebar
+			className={props.visibility ? "Sidebar active" : "Sidebar"}
+			hide={props.hideSidebar}
+			switchTheme={props.switchTheme}
+		/> // most function is given by navbar, ie props
+	);
+};
+
+export default SidebarContainer; // to be used by the navbar
--- site/client/src/index.js
+++ site/client/src/index.js
@@ -1,4 +1,11 @@
 import ReactDOM from "react-dom";
+// necessary to use react - allows it to be rendered. Since every file is a child to this one, no
+// other files need to do this really.
+
+import { useState } from "react";
+
+// import the theme file containing the information about what colours to use with what theme
+import "./theme.scss";
 
 // router tools
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -6,10 +13,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // pages
 import Homepage from "./pages/homepage";
 
+// navbar
+import Navbar from "./components/large/navbar";
+
 const App = () => {
 	// default/base function component
+
+	const [darkTheme, setDarkTheme] = useState(true);
+
+	const switchTheme = () => {
+		setDarkTheme(!darkTheme);
+		const body = document.querySelector("body");
+		body.className = darkTheme ? "light" : "dark";
+	};
+
 	return (
 		<Router>
+			<Navbar switchTheme={switchTheme} />
 			<Routes>
 				<Route path="/" exact component={Homepage} />
 				{/* home page here on the default url */}
